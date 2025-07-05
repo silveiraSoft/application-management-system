3bbae0dce8a42d20d64de1a29355e0a924d88cae
silveiraSoft <adalbertosn1982@gmail.com>
2025-07-05 13:38:54 -0400

    fist commit

---
+++ src/test/java/com/adalbertosn1982/applicationmanagementsystem/ApplicationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adalbertosn1982.applicationmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
