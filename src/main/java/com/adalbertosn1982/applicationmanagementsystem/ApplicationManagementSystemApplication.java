3c713543a8b736cf63bb42862d09780707ac468a
silveiraSoft <adalbertosn1982@gmail.com>
2025-07-05 14:42:25 -0400

    fixing lombok error

--- src/main/java/com/adalbertosn1982/applicationmanagementsystem/ApplicationManagementSystemApplication.java
+++ src/main/java/com/adalbertosn1982/applicationmanagementsystem/ApplicationManagementSystemApplication.java
@@ -1,13 +1,45 @@
 package com.adalbertosn1982.applicationmanagementsystem;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.lang.management.ManagementFactory;
+import java.text.NumberFormat;
+
 
 @SpringBootApplication
+@Slf4j
 public class ApplicationManagementSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationManagementSystemApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ApplicationManagementSystemApplication.class, args);
+    }
+
+    public static void printInfo() {
+        Runtime runtime = Runtime.getRuntime();
+
+        final NumberFormat format = NumberFormat.getInstance();
+        final long maxMemory = runtime.maxMemory();
+        final long allocatedMemory = runtime.totalMemory();
+        final long freeMemory = runtime.freeMemory();
+        final long mb = 1024L * 1024L;
+        final String mega = " MB";
+        log.info("========================== Memory Info ==========================");
+        log.info("name = " + ManagementFactory.getRuntimeMXBean().getName());
+        log.info("Free memory: " + format.format(freeMemory / mb) + mega);
+        log.info("Allocated memory: " + format.format(allocatedMemory / mb) + mega);
+        log.info("Max memory: " + format.format(maxMemory / mb) + mega);
+        log.info("Total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / mb) + mega);
+        log.info("=================================================================");
+    }
+
+    @Bean
+    public CommandLineRunner memInfoRunner() {
+        return args -> printInfo();
     }
 
 }
